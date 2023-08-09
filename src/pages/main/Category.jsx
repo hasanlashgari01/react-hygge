bc706c4c106b78f134a198793ea4a5375ac28bd6
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-09 22:45:07 +0330

    refactor: category

--- src/pages/main/Category.jsx
+++ src/pages/main/Category.jsx
@@ -37,48 +37,21 @@ function CategoryProducts() {
                         <SubTitle subtitle={`- ${category.title} Products`} />
                         <Title title={`Explore the ${category.title} Products`} />
                     </div>
-                    <>
-                        <div className="flex flex-col justify-between items-center gap-8 w-full mt-16">
-                            <div className="flex desktop:hidden justify-between items-center w-full">
-                                <div className="dropdown-wrapper">
-                                    <span className="text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
-                                        Filter By
-                                    </span>
-                                    <svg className="w-4 h-4 text-black dark:text-white">
-                                        <use href="#arrow-down"></use>
-                                    </svg>
-                                </div>
-                                <div className="dropdown-wrapper">
-                                    <span className="text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
-                                        Sort By
-                                    </span>
-                                    <svg className="w-4 h-4 text-black dark:text-white">
-                                        <use href="#arrow-down"></use>
-                                    </svg>
-                                </div>
-                            </div>
-
-                            <div className="flex justify-between items-center w-full">
-                                <div className="flex flex-wrap desktop:flex-nowrap desktop:flex-row items-center gap-8 w-full desktop:w-fit">
-                                    <div className="dropdown-wrapper w-full tablet:flex-1 desktop:flex-grow-0">
-                                        <span className="w-[120px] text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
-                                            Color
+                    {products.length ? (
+                        <>
+                            <div className="flex flex-col justify-between items-center gap-8 w-full mt-16">
+                                <div className="flex desktop:hidden justify-between items-center w-full">
+                                    <div className="dropdown-wrapper">
+                                        <span className="text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
+                                            Filter By
                                         </span>
                                         <svg className="w-4 h-4 text-black dark:text-white">
                                             <use href="#arrow-down"></use>
                                         </svg>
                                     </div>
-                                    <div className="dropdown-wrapper w-full tablet:flex-1 desktop:flex-grow-0">
-                                        <span className="w-[120px] text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
-                                            Use Case
-                                        </span>
-                                        <svg className="w-4 h-4 text-black dark:text-white">
-                                            <use href="#arrow-down"></use>
-                                        </svg>
-                                    </div>
-                                    <div className="dropdown-wrapper w-full tablet:flex-1 desktop:flex-grow-0">
-                                        <span className="w-[120px] text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
-                                            Price Range
+                                    <div className="dropdown-wrapper">
+                                        <span className="text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
+                                            Sort By
                                         </span>
                                         <svg className="w-4 h-4 text-black dark:text-white">
                                             <use href="#arrow-down"></use>
@@ -86,27 +59,54 @@ function CategoryProducts() {
                                     </div>
                                 </div>
 
-                                <div className="hidden desktop:inline-flex dropdown-wrapper">
-                                    <span className="text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
-                                        Sort By
-                                    </span>
-                                    <svg className="w-4 h-4 text-black dark:text-white">
-                                        <use href="#arrow-down"></use>
-                                    </svg>
+                                <div className="flex justify-between items-center w-full">
+                                    <div className="flex flex-wrap desktop:flex-nowrap desktop:flex-row items-center gap-8 w-full desktop:w-fit">
+                                        <div className="dropdown-wrapper w-full tablet:flex-1 desktop:flex-grow-0">
+                                            <span className="w-[120px] text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
+                                                Color
+                                            </span>
+                                            <svg className="w-4 h-4 text-black dark:text-white">
+                                                <use href="#arrow-down"></use>
+                                            </svg>
+                                        </div>
+                                        <div className="dropdown-wrapper w-full tablet:flex-1 desktop:flex-grow-0">
+                                            <span className="w-[120px] text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
+                                                Use Case
+                                            </span>
+                                            <svg className="w-4 h-4 text-black dark:text-white">
+                                                <use href="#arrow-down"></use>
+                                            </svg>
+                                        </div>
+                                        <div className="dropdown-wrapper w-full tablet:flex-1 desktop:flex-grow-0">
+                                            <span className="w-[120px] text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
+                                                Price Range
+                                            </span>
+                                            <svg className="w-4 h-4 text-black dark:text-white">
+                                                <use href="#arrow-down"></use>
+                                            </svg>
+                                        </div>
+                                    </div>
+
+                                    <div className="hidden desktop:inline-flex dropdown-wrapper">
+                                        <span className="text-grey-dark-100 dark:text-grey-light-100 text-xl/[32px] font-bold">
+                                            Sort By
+                                        </span>
+                                        <svg className="w-4 h-4 text-black dark:text-white">
+                                            <use href="#arrow-down"></use>
+                                        </svg>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        {products.length ? (
                             <div className="grid grid-cols-1 mobile:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 gap-x-12 laptop:gap-x-14 desktop:gap-x-12 gap-y-16 mt-14">
                                 {products &&
                                     products.map(product => (
                                         <Product key={product._id} product={product} />
                                     ))}
                             </div>
-                        ) : (
-                            !isPending && <ErrorMessage title="There is no product" />
-                        )}
-                    </>
+                        </>
+                    ) : (
+                        !isPending && <ErrorMessage title="There is no product" />
+                    )}
                 </div>
                 <NewsLetter />
             </div>
