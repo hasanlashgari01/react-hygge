4caeb11e6d878e4a27ebb109d106fe5aa5cb9454
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-09 19:49:32 +0330

    create go home buttom on 404 page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,20 @@
 import Header from "../layouts/main/Header";
 import Footer from "../layouts/main/Footer";
+import SmoothScrollLink from "./SmoothScrollLink";
 
 function NotFound() {
     return (
         <>
             <Header />
-            <div className="space-container flex justify-center items-center py-60 text-6xl font-black text-green-100">404 Not Found 404</div>
+            <div className="space-container flex flex-col items-center h-screen">
+                <h1 className="my-28 text-6xl font-black text-green-100">404 Not Found 404</h1>
+                <SmoothScrollLink
+                    to="/"
+                    className="text-lg font-bold bg-green-10 text-green-100 py-4 px-8 rounded-lg hover:scale-125 transition-all duration-200 ease-linear">
+                    {" "}
+                    Go back home{" "}
+                </SmoothScrollLink>
+            </div>
             <Footer />
         </>
     );
