843ddee9751885323ea8378680879b510aa7a157
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-04 12:00:53 +0330

    fix: get details product in likes and bookmarks on panel user

--- src/pages/panel-user/Likes.jsx
+++ src/pages/panel-user/Likes.jsx
@@ -11,7 +11,7 @@ function Likes() {
             {likes.length >= 1 ? (
                 <div className="grid grid-cols-1 xmobile:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-2 desktop:grid-cols-3 gap-x-12 laptop:gap-x-14 desktop:gap-x-12 gap-y-16">
                     {likes.map(like => (
-                        <Product key={like._id} product={like} />
+                        <Product key={like._id} {...like} />
                     ))}
                 </div>
             ) : (
